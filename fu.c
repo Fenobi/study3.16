9911cf8ee1f2545a882bac2d3f1c21c9551cbae4
付青云 <1366773912@qq.com>
2022-03-16 22:31:20 +0800

    添加项目文件。

---
+++ fu.c
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+//int main()
+//{
+//	int i, j;
+//	int sum = 0;
+//	int n = 0;
+//	scanf("%d", &n);
+//	for (j = 1; j <= n; j++)
+//	{
+//		int ret = 1;
+//		for (i = 1; i <= j; i++)
+//		{
+//			ret *= i;
+//		}
+//		sum += ret;
+//	}
+//	printf("%d\n", sum);
+//	return 0;
+//}
+
+//int main()
+//{
+//	unsigned char a = 26;
+//	printf("%c\n", a);
+//	return 0;
+//}
+
+int main()
+{
+	char a = 26;
+	putchar(a);
+	return 0;
+}
